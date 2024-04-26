8c026355e1d8758f6f18ce860f9688c757f076ec
Daniil Kalinin <dan_kalinin_03@mail.ru>
2024-04-26 20:50:03 +0300

    test

--- main.cpp
+++ main.cpp
@@ -19,7 +19,7 @@ int main(int argc, char* argv[]) {
     numbers.push_back(rawInput);
   }
 
-  for (int i = 0; i < numbers.size(); i++){
+  for (size_t i = 0; i < numbers.size(); i++){
     cout << numbers[i] << endl;
   }
 
